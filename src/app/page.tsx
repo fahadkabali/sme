ceb9a3394fc111d7a72f5b2fec9ec9ce378434d8
fahadkabali <kabalifahad@gmail.com>
2024-07-01 16:55:05 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
-import Image from "next/image";
-import { LoginForm } from "./components/forms/Login";
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LoginForm/>
-    </main>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
   );
 }
