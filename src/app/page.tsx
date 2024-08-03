f38f5d095e87f5e99a209e40684785630a5b830f
fahadkabali <kabalifahad@gmail.com>
2024-08-04 02:08:25 +0300

    update

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head';
-import utilStyles from '../styles/utils.module.css';
-import withAuth from '@components/withAuth';
-
-export default function Home() {
-  return (
-    <withAuth/>
-  );
-}
