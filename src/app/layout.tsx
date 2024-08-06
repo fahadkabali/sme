7538c8521c7c9218a9389d7f7451ff2244950aa1
fahadkabali <kabalifahad@gmail.com>
2024-08-06 18:16:42 +0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,15 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import Link from "next/link";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -21,16 +30,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-      {children}
+  <ClerkProvider>
+    <html lang='en'>
+      <body className={inter.className}>
+        <nav className='flex justify-between items-center h-[10vh] px-8 border-b-[1px]'>
+          <Link href='/' className='text-xl font-extrabold text-blue-700'>
+          Invoicer
+          </Link>
+          <div className='flex items-center gap-5'>
+                                    {/*-- if user is signed out --*/}
+            <SignedOut>
+            <SignInButton mode='modal' />
+            </SignedOut>
+                                    {/*-- if user is signed in --*/}
+            <SignedIn>
+            <Link href='/dashboard' className=''>
+            Dashboard
+            </Link>
+            <UserButton showName />
+            </SignedIn>
+          </div>
+        </nav>
+
+                        {children}
       </body>
     </html>
-  );
+  </ClerkProvider>
+);
 }
